6988ef91ab25aeeaeae95ff07c718c18b973a3ba
Joeykmoore13 <168593468+Joeykmoore13@users.noreply.github.com>
2026-04-23 16:06:52 -0600

    Created the Pizza, Directories, and Dependencies projects and added the related code and files to each.

---
+++ DotNetDependencies/Program.cs
@@ -0,0 +1,45 @@
+﻿using Humanizer;
+using System.Diagnostics;
+Console.WriteLine("Quantities:");
+HumanizeQuantities();
+
+Console.WriteLine("\nDate/Time Manipulation:");
+HumanizeDates();
+
+
+Debug.WriteLine($"Entering {nameof(Fibonacci)} method");
+
+int result = Fibonacci(5);
+Console.WriteLine(result);
+// Debug.Assert(result == 10, "Ran code here");
+
+static int Fibonacci(int n)
+{
+    int n1 = 0;
+    int n2 = 1;
+    int sum;
+
+    for (int i = 2; i < n; i++)
+    {
+        sum = n1 + n2;
+        n1 = n2;
+        n2 = sum;
+    }
+
+    return n == 0 ? n1 : n2;
+}
+
+static void HumanizeQuantities()
+{
+    Console.WriteLine("case".ToQuantity(0));
+    Console.WriteLine("case".ToQuantity(1));
+    Console.WriteLine("case".ToQuantity(5));
+}
+
+static void HumanizeDates()
+{
+    Console.WriteLine(DateTime.UtcNow.AddHours(-24).Humanize());
+    Console.WriteLine(DateTime.UtcNow.AddHours(-2).Humanize());
+    Console.WriteLine(TimeSpan.FromDays(1).Humanize());
+    Console.WriteLine(TimeSpan.FromDays(16).Humanize());
+}
